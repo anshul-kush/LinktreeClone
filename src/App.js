394387d6c1970df221b715b517acdfbf7f9d19eb
anshul-kush <92118769+anshul-kush@users.noreply.github.com>
2022-05-25 01:38:16 +0530

    Final Update

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p><strong>Anshul Kushwaha</strong></p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/anshulcivil/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Linkedin
         </a>
+        <a className="App-link" href="https://www.instagram.com/anshulthakur111/" target="_blank" rel="noopener noreferrer"> Instagram </a>
+        <a href="tel:9953709702">9953709702</a>
+        
+        {/* <img class="circular--portrait" src="https://d1fdloi71mui9q.cloudfront.net/p8NgH3hcQaGTXLSESeSg_uPOgU2I29wNEiwwd" alt="Anshul Kushwaha" /> */}
       </header>
+
     </div>
   );
 }
