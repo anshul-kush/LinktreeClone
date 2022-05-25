5e4c46af2860301bfddf509be4922bff071f8747
anshul-kush <92118769+anshul-kush@users.noreply.github.com>
2022-05-25 16:16:41 +0530

    trying

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://lh3.googleusercontent.com/a-/AOh14Giwh80podIxiPsVcMuQwoTW0nzFHNYXH9fcOlIHDA=s288-p-rw-no" className="App-logo" alt="logo" />
         <p><strong>Anshul Kushwaha</strong></p>
+        <a href="tel:9953709702" class="btn btn-primary mx-1 my-1 active" aria-current="page">9953709702</a>
         <a
-          className="App-link"
-          href="https://www.linkedin.com/in/anshulcivil/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Linkedin
-        </a>
-        <a className="App-link" href="https://www.instagram.com/anshulthakur111/" target="_blank" rel="noopener noreferrer"> Instagram </a>
-        <a href="tel:9953709702">9953709702</a>
+          className="btn btn-primary mx-1 my-1 active" href="https://www.linkedin.com/in/anshulcivil/" target="_blank"      rel="noopener noreferrer">Linkedin</a>
         
+        <a href="https://www.instagram.com/anshulthakur111/" class="btn btn-primary mx-1 my-1 active" aria-current="page" target="_blank" rel="noopener noreferrer">Instagram</a>
+      
+
+
+
         {/* <img class="circular--portrait" src="https://d1fdloi71mui9q.cloudfront.net/p8NgH3hcQaGTXLSESeSg_uPOgU2I29wNEiwwd" alt="Anshul Kushwaha" /> */}
+
       </header>
+      <p class="sticky-sm-bottom">Built by Ak</p>
 
     </div>
   );
