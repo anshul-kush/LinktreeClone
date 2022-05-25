692fe114c137fa9b5f37ef4df9529825201fe46a
anshul-kush <92118769+anshul-kush@users.noreply.github.com>
2022-05-25 17:51:13 +0530

    try2

--- src/App.js
+++ src/App.js
@@ -9,21 +9,20 @@ function App() {
       <header className="App-header">
         <img src="https://lh3.googleusercontent.com/a-/AOh14Giwh80podIxiPsVcMuQwoTW0nzFHNYXH9fcOlIHDA=s288-p-rw-no" className="App-logo" alt="logo" />
         <p><strong>Anshul Kushwaha</strong></p>
-        <a href="tel:9953709702" class="btn btn-primary mx-1 my-1 active" aria-current="page">9953709702</a>
+        <a href="tel:9953709702" className="btn btn-primary mx-1 my-1 active" aria-current="page">9953709702</a>
         <a
           className="btn btn-primary mx-1 my-1 active" href="https://github.com/anshul-kush" target="_blank" rel="noopener noreferrer">GitHub</a>
         <a
           className="btn btn-primary mx-1 my-1 active" href="https://www.linkedin.com/in/anshulcivil/" target="_blank"      rel="noopener noreferrer">Linkedin</a>
         
-        <a href="https://www.instagram.com/anshulthakur111/" class="btn btn-primary mx-1 my-1 active" aria-current="page" target="_blank" rel="noopener noreferrer">Instagram</a>
+        <a href="https://www.instagram.com/anshulthakur111/" className="btn btn-primary mx-1 my-1 active" aria-current="page" target="_blank" rel="noopener noreferrer">Instagram</a>
       
 
 
 
-        {/* <img class="circular--portrait" src="https://d1fdloi71mui9q.cloudfront.net/p8NgH3hcQaGTXLSESeSg_uPOgU2I29wNEiwwd" alt="Anshul Kushwaha" /> */}
+        {/* <img className="circular--portrait" src="https://d1fdloi71mui9q.cloudfront.net/p8NgH3hcQaGTXLSESeSg_uPOgU2I29wNEiwwd" alt="Anshul Kushwaha" /> */}
 
       </header>
-      <p class="sticky-sm-bottom">Built by Ak</p>
 
     </div>
   );
